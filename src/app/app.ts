6e418300f743e2b4bac50ede2af1f01a9d9be66b
Dhruv Patel <dhruvpatelcode@gmail.com>
2025-09-05 11:29:36 -0400

    feat: implement routing for inventory component and update app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterOutlet, RouterModule]
 })
 export class App {
   protected readonly title = signal('gadget-shop');
